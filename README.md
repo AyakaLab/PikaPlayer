bac46577b4afe2a128f2608fa82599a44588756b
Ayaka Neko <neko@ayaka.moe>
2020-10-24 15:21:49 +0800

    :sparkles: 仿照 Aplayer 完成了一些测试代码

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 <h1 align="center">PikaPlayer</h1>
 <p align="center">A Web Music Player</p>
+<p align="center">
+    <img src="https://github.com/AyakaLab/PikaPlayer/workflows/Node%20Test/badge.svg" />
+    <img src="https://github.com/AyakaLab/PikaPlayer/workflows/Node%20Build/badge.svg" />
+</p>
--- src/js/pikaplayer.js
+++ src/js/pikaplayer.js
@@ -1,1 +1,16 @@
+import handleOption from './options';
+class PikaPlayer {
+    constructor(options) {
+        this.options = handleOption(options)
+        this.container = this.options.container
+        const div = document.createElement('div')
+        this.container.appendChild(div)
+        const img = document.createElement('img')
+        // img.src = this.options.audio[0].cover
+        div.appendChild(img)
+    }
+}
+
 console.log('linked')
+
+export default PikaPlayer;
